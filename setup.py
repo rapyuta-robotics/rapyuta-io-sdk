1fe1d2894549550d36703b88295ee5540b8fe698
Pallab Pain <pallabkumarpain@gmail.com>
2023-09-26 14:07:06 +0530

    chore: removes Python 3.7 support

--- setup.py
+++ setup.py
@@ -14,11 +14,10 @@ setup(
     author="Rapyuta Robotics",
     author_email="opensource@rapyuta-robotics.com",
     packages=find_packages(include=["rapyuta_io*"]),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     license="Apache 2.0",
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
